85ab77b80d8d3fbbc350699add0c505b49c9e02c
Orlando Signer <orlando.signer@gmail.com>
2018-04-14 14:18:05 +0200

    Missing @Ignore import added

--- src/test/java/ch/osi/jglove/preprocess/ProcessWikiXmlFileTest.java
+++ src/test/java/ch/osi/jglove/preprocess/ProcessWikiXmlFileTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ProcessWikiXmlFileTest {
