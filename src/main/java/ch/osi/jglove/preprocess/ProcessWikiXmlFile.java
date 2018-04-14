135aed9b818a473636fdb58fdfd418831956943c
Orlando Signer <orlando.signer@gmail.com>
2018-04-14 14:17:28 +0200

    Change from XMLEventReader to XMLStreamReader Use AssertJ instead Hamcrest

--- src/main/java/ch/osi/jglove/preprocess/ProcessWikiXmlFile.java
+++ src/main/java/ch/osi/jglove/preprocess/ProcessWikiXmlFile.java
@@ -1,50 +1,65 @@
 package ch.osi.jglove.preprocess;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 
-import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.XMLStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProcessWikiXmlFile {
 
-	public Stream<WikiPage> process(String wikiFile) {
+	private static final Logger LOG = LoggerFactory.getLogger(ProcessWikiXmlFile.class);
+
+	public Stream<WikiPage> process(File wikiFile) {
 		Builder<WikiPage> builder = Stream.builder();
 		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
 		try {
-			XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(wikiFile));
+			XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileInputStream(wikiFile),
+					StandardCharsets.UTF_8.name());
 			String title = null;
-			String text = null;
-			while (xmlEventReader.hasNext()) {
-				XMLEvent xmlEvent = xmlEventReader.nextEvent();
-				if (xmlEvent.isStartElement()) {
-					StartElement startElement = xmlEvent.asStartElement();
-					String eventName = startElement.getName().getLocalPart();
-					switch (eventName) {
+			StringBuilder text = null;
+			while (xmlStreamReader.hasNext()) {
+				xmlStreamReader.next();
+				// XMLEvent xmlEvent = xmlStreamReader.nextEvent();
+				if (xmlStreamReader.isStartElement()) {
+					switch (xmlStreamReader.getLocalName()) {
 					case "page":
 						title = null;
 						text = null;
 						break;
 					case "title":
-						title = xmlEventReader.nextEvent().asCharacters().getData();
+						xmlStreamReader.next();
+						title = xmlStreamReader.getText();
 						break;
 					case "text":
-						text = xmlEventReader.nextEvent().asCharacters().getData();
+						xmlStreamReader.next();
+						if (xmlStreamReader.isCharacters()) {
+							text = new StringBuilder();
+							do {
+								text.append(xmlStreamReader.getText());
+								xmlStreamReader.next();
+							} while (xmlStreamReader.isCharacters());
+						}
 						break;
 					default:
 						continue;
 					}
-				}
-				if (xmlEvent.isEndElement()) {
-					EndElement endElement = xmlEvent.asEndElement();
-					if (endElement.getName().getLocalPart().equals("page")) {
-						builder.add(new WikiPage(title, text));
+				} else if (xmlStreamReader.isEndElement()) {
+					if (xmlStreamReader.getLocalName().equals("page")) {
+						if (text == null) {
+							LOG.info("No text for Page {}, ignoring it", title);
+						} else {
+							LOG.debug("Add wiki-page {}", title);
+							builder.add(new WikiPage(title, text.toString()));
+						}
 					}
 				}
 			}
--- src/test/java/ch/osi/jglove/preprocess/ProcessWikiXmlFileTest.java
+++ src/test/java/ch/osi/jglove/preprocess/ProcessWikiXmlFileTest.java
@@ -1,9 +1,9 @@
 package ch.osi.jglove.preprocess;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,15 +14,49 @@ public class ProcessWikiXmlFileTest {
 
 	@Test
 	public void processDeWiki37Articles() {
-		String file = "src/test/resources/ch/osi/jglove/preprocess/dewiki-37-articles.xml";
+		File file = getFile("dewiki-37-articles.xml");
 		ProcessWikiXmlFile processor = new ProcessWikiXmlFile();
 
 		Stream<WikiPage> output = processor.process(file);
-		assertNotNull(output);
+		assertThat(output).isNotNull();
 		List<WikiPage> pages = output.collect(Collectors.toList());
 
-		assertThat(pages.size(), is(36));
+		assertThat(pages).size().isEqualTo(36);
 		WikiPage first = pages.get(0);
-		assertThat(first.getTitle(), is("Alan Smithee"));
+		assertThat(first.getTitle()).isEqualTo("Alan Smithee");
+		assertThat(first.getText()).startsWith("'''Alan Smithee'''").endsWith("[[Kategorie:Werk von Alan Smithee]]");
+		assertThat(first.getText().length()).isGreaterThan(1000);
+	}
+
+	@Test
+	public void processNoTextArticle() {
+		File file = getFile("dewiki-no-text-article.xml");
+		ProcessWikiXmlFile processor = new ProcessWikiXmlFile();
+
+		Stream<WikiPage> output = processor.process(file);
+		assertThat(output).isNotNull();
+		List<WikiPage> pages = output.collect(Collectors.toList());
+
+		assertThat(pages).isEmpty();
+	}
+
+	@Test
+	@Ignore
+	public void procesFullWiki() {
+		File file = Paths.get("F:", "wiki", "dewiki-20180401-pages-articles.xml", "dewiki-20180401-pages-articles.xml")
+				.toFile();
+		ProcessWikiXmlFile processor = new ProcessWikiXmlFile();
+
+		Stream<WikiPage> output = processor.process(file);
+		assertThat(output).isNotNull();
+		List<WikiPage> pages = output.collect(Collectors.toList());
+
+		assertThat(pages.size()).isGreaterThan(10000);
+		WikiPage first = pages.get(0);
+		assertThat(first.getTitle()).isEqualTo("Alan Smithee");
+	}
+
+	private File getFile(String filename) {
+		return Paths.get("src", "test", "resources", "ch", "osi", "jglove", "preprocess", filename).toFile();
 	}
 }
